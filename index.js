51a9083bf23b13127201958470743238a37476e0
Kesar <senkevich_danil@mail.ru>
2023-03-04 14:04:05 +0300

    feat: add animations

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const h12 = document.querySelector('.introduce-h1');
-
+const ex = document.querySelector('.code-ex')
 document.onreadystatechange = function () {
     if (document.readyState == "interactive") {
         h12.classList.add('hover1');
     }
   }
+
+
+function isVisible(elem) {
+
+    let coords = elem.getBoundingClientRect();
+  
+    let windowHeight = document.documentElement.clientHeight;
+    let topVisible = coords.top > 0 && coords.top < windowHeight;
+    let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+  
+    return topVisible || bottomVisible;
+  }
+function showVisible() {
+    for (let line of document.querySelectorAll('.line')) {
+      if (isVisible(line)) {
+        line.classList.add('line-hover');
+      }
+    }
+    if(isVisible(ex)) {
+        ex.classList.add('ex-hover');
+    }
+  }
+  showVisible();
+  window.onscroll = showVisible;
